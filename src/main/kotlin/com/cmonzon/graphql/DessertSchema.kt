3e7e0cf0d0f822c953cbf76ca51f1c677f53d37e
Carlos Monzon <carlosmonzonsalvador@gmail.com>
2022-03-11 20:54:31 +1000

    - Adding deleteDessert & updateDessert mutation

--- src/main/kotlin/com/cmonzon/graphql/DessertSchema.kt
+++ src/main/kotlin/com/cmonzon/graphql/DessertSchema.kt
@@ -9,7 +9,7 @@ import java.util.*
 fun SchemaBuilder.dessertSchema() {
     val repository = DessertRepository()
     inputType<DessertInputDTO> {
-        description = "This is the data transfer object of the dessert"
+        description = "This is the data transfer object of the dessert without the identifier"
     }
 
     type<Dessert> {
@@ -34,6 +34,22 @@ fun SchemaBuilder.dessertSchema() {
             val uid = UUID.randomUUID().toString()
             val dessert = Dessert(uid, dessertInputDTO.name, dessertInputDTO.description, dessertInputDTO.imageUrl)
             repository.add(dessert)
+        }
+    }
+
+    mutation("deleteDessert") {
+        description = "delete a dessert"
+        resolver { dessertId: String ->
+            repository.delete(dessertId)
+        }
+    }
+
+    mutation("updateDessert") {
+        description = "update a dessert"
+        resolver { dessertId: String, dessertInputDTO: DessertInputDTO ->
+            val dessert =
+                Dessert(dessertId, dessertInputDTO.name, dessertInputDTO.description, dessertInputDTO.imageUrl)
+            repository.update(dessert)
         }
     }
 }
